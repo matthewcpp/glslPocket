dc3b855cbed6f7b1d8df796cc0ee062fec5acbf0
Matthew LaRocca <mlarocca.cpp@gmail.com>
2025-08-16 20:14:51 -0700

    Remove unneeded files

--- shadertoy/module.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "graph/typeRegistry.hpp"
-#include "graph/schemaRegistry.hpp"
-
-namespace shadertoy {
-    void initModule(graph::TypeRegistry& typeRegistry, graph::SchemaRegistry& schemaRegistry);
-}
